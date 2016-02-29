99d47784773aeb9d51459a345d78c6d9b022b8a2
Jarvis Zhang <345054597@qq.com>
2016-02-29 13:26:18 +0800

    fix
    修复多个课程情况下的显示错误

--- courseMaker.js
+++ courseMaker.js
@@ -14,6 +14,7 @@ function  tableToJson(){
 	$('table').children('tbody').children('tr:first').each(function(){
 		data_=[];
 		data_[0]=40;
+		data_[1]=40;
 		data__=[];
 		$(this).children('td:first').each(function(){
 			data_0=[];
@@ -35,6 +36,7 @@ function  tableToJson(){
 	$('table').children('tbody').children('tr:first').nextAll().each(function(){
 		data_=[];
 		data_[0]=130;
+		data_[1]=130;
 		data__=[];
 		$(this).children('td:first').each(function(){
 			data_0=[];
@@ -65,7 +67,7 @@ function  tableToJson(){
 		data_[data_.length]=data__;
 		data[data.length]=data_;
 	});
-	//console.log(data)
+	console.log(data)
 	return data;
 }
 function cleanHtml(str){
@@ -98,22 +100,22 @@ function  makeCourse(data,pic) {
 	var canvasE = document.createElement("canvas");
 	document.body.appendChild(canvasE);
 	//canvasE.style.display="none";
-	var heigt_=0,width_=0;
+	var height_=0,width_=0;
 	for (var i = 0; i < data.length; i++) {
-		for(var a=0;a<data[i][1].length;a++){
-			heigt_+=data[i][0];
+		for(var a=0;a<data[i][2].length;a++){
+			height_+=data[i][1];
 			break;
 		}
 	}
 	for (var i = 0; i < data.length; i++) {
 		var mm=0;
-		for(var a=0;a<data[i][1].length;a++){
-			mm+=data[i][1][a][0];
+		for(var a=0;a<data[i][2].length;a++){
+			mm+=data[i][2][a][0];
 		}
 		if(mm>width_)width_=mm;
 	}
 	canvasE.width=width_;
-	canvasE.height=heigt_;
+	canvasE.height=height_;
 	var canvas=canvasE.getContext('2d');
 	canvas.font="normal normal 400 30px Microsoft Yahei";
 	canvas.fillText('_(:3 」∠ )_ 翼宝正在努力绘制你的课表...',(width_-canvas.measureText('_(:3 」∠ )_ 翼宝正在努力绘制你的课表...').width)/2,canvasE.height/2-40);
@@ -125,29 +127,40 @@ function  makeCourse(data,pic) {
 		canvas.strokeStyle='#DDDDDD';
 		canvas.lineWidth=0;
 		var maxWidth=[0,0,0,0,0,0,0,0];
-
+		var maxHeight=[0,0,0,0,0,0];
 		for (var i = 0; i < data.length; i++) {
-			for(var a=0;a<data[i][1].length;a++){
+
+			for(var a=0;a<data[i][2].length;a++){
+				var temp0=0;
 				var tt=0;
-				if(data[i][1][a][1])tt=data[i][1][a][1].length;
-				if(maxWidth[a]<data[i][1][a][0])maxWidth[a]=data[i][1][a][0];
+				if(data[i][2][a][1])tt=data[i][2][a][1].length;
+				if(maxWidth[a]<data[i][2][a][0])maxWidth[a]=data[i][2][a][0];
 				for(var b=0;b<tt;b++){
-					if(data[i][1][a][1][0].length>1){
+					if(data[i][2][a][1][0].length>1){
 						canvas.font="normal normal 700 14px Microsoft Yahei";
-						var temp=canvas.measureText(data[i][1][a][1][b][0]).width+30;
+						var temp=canvas.measureText(data[i][2][a][1][b][0]).width+30;
 						if(maxWidth[a]<temp)maxWidth[a]=temp;
 						canvas.font="normal normal 400 14px Microsoft Yahei";
-						temp=canvas.measureText(data[i][1][a][1][b][1]).width+20+canvas.measureText(data[i][1][a][1][b][2]).width+30;
+						temp=canvas.measureText(data[i][2][a][1][b][1]).width+20+canvas.measureText(data[i][2][a][1][b][2]).width+30;
 						if(maxWidth[a]<temp)maxWidth[a]=temp;
+						temp0+=60;
 					}
 				}
+				if(maxHeight[i]<temp0)maxHeight[i]=temp0;
 			}
+			if(maxHeight[i]<data[i][1])maxHeight[i]=data[i][1];
 		}
 		width_=0;
 		for(var a=0;a<maxWidth.length;a++){
 			width_+=maxWidth[a];
 		}
+		height_=0;
+		for(var a=0;a<maxHeight.length;a++){
+			height_+=maxHeight[a];
+		}
+		console.log(width_,height_,maxHeight)
 		canvasE.width=width_;
+		canvasE.height=height_;
 		canvas.strokeStyle='#DDDDDD';
 		canvas.lineWidth=0;
 		for (var i = 0; i < data.length; i++) {
@@ -155,51 +168,49 @@ function  makeCourse(data,pic) {
 				canvas.fillStyle='#F9F9F9';
 			}
 			else{canvas.fillStyle='#FFF';}
-			canvas.fillRect(0,yTable,canvasE.clientWidth,data[i][0]);
-			yTable+=data[i][0];
+			canvas.fillRect(0,yTable,canvasE.clientWidth,maxHeight[i]);
+			yTable+=maxHeight[i];
 			canvas.beginPath();
 			canvas.moveTo(0,yTable);
 			canvas.lineTo(canvasE.clientWidth,yTable);
 			canvas.stroke();
 			//console.log(data[i][0]);
 			xTable=0;
-
-
 			//console.log(data);
-			for(var a=0;a<data[i][1].length;a++){
+			for(var a=0;a<data[i][2].length;a++){
 				canvas.font="normal normal 400 14px Microsoft Yahei";
 				canvas.fillStyle="#000";
-				var y_=yTable-(data[i][0])+14+5;
+				var y_=yTable-(maxHeight[i])+14+5;
 				var tt=0;
-				if(data[i][1][a][1])tt=data[i][1][a][1].length;
+				if(data[i][2][a][1])tt=data[i][2][a][1].length;
 				for(var b=0;b<tt;b++){
 					//console.log(data[i][1][a][1])
-					if(data[i][1][a][1][0].length>1){
+					if(data[i][2][a][1][0].length>1){
 						canvas.font="normal normal 700 14px Microsoft Yahei";
-						canvas.fillText(data[i][1][a][1][b][0],xTable+5,y_);
+						canvas.fillText(data[i][2][a][1][b][0],xTable+5,y_);
 						canvas.font="normal normal 400 14px Microsoft Yahei";
-						canvas.fillText(data[i][1][a][1][b][1],xTable+5,y_+16);
-						canvas.fillText(data[i][1][a][1][b][2],xTable+5+canvas.measureText(data[i][1][a][1][b][1]).width+20,y_+16);
-						canvas.fillText(data[i][1][a][1][b][3],xTable+5,y_+16+16);
-						y_+=16*3+5;
+						canvas.fillText(data[i][2][a][1][b][1],xTable+5,y_+18);
+						canvas.fillText(data[i][2][a][1][b][2],xTable+5+canvas.measureText(data[i][2][a][1][b][1]).width+20,y_+18);
+						canvas.fillText(data[i][2][a][1][b][3],xTable+5,y_+18+18);
+						y_+=18*3+5;
 					}
 					else{
-						if (data[i][1][a][2]=="center") {
-							canvas.fillText(data[i][1][a][1],xTable+5,y_+(data[i][0]-14)/2-5);
+						if (data[i][2][a][2]=="center") {
+							canvas.fillText(data[i][2][a][1],xTable+5,y_+(data[i][0]-14)/2-5);
 						}
-						else if(data[i][1][a][2]=="vertical"){
-							for(var c=0;c<data[i][1][a][1].length;c++){
-								canvas.fillText(data[i][1][a][1].substring(c,c+1),xTable+12,y_+5+c*22);
+						else if(data[i][2][a][2]=="vertical"){
+							for(var c=0;c<data[i][2][a][1].length;c++){
+								canvas.fillText(data[i][2][a][1].substring(c,c+1),xTable+12,y_+5+c*22);
 							}
 						}
-						else{canvas.fillText(data[i][1][a][1],xTable+5,y_);}
+						else{canvas.fillText(data[i][2][a][1],xTable+5,y_);}
 						break;
 					}
 				}
 				//console.log(canvas.measureText(data[i][1][a][1][b][0]).width)
 				xTable+=maxWidth[a];
 				canvas.beginPath();
-				canvas.moveTo(xTable,yTable-data[i][0]);
+				canvas.moveTo(xTable,yTable-maxHeight[i]);
 				canvas.lineTo(xTable,yTable);
 				canvas.stroke();
 			}
